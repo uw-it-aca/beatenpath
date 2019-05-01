fa8e0caf02cc689b5ba31a61b0bc3f7d4001e7b0
Charlon Palacay <charlon@gmail.com>
2019-05-01 12:12:18 -0700

    Move json load funtion to Vue component.

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -27,10 +27,11 @@
       </div>
     </div>
 
-
     <div><img alt="Vue logo" src="@/static/img/logo.png"></div>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <MyCard/>
+
   </div>
 
 </template>
@@ -44,7 +45,26 @@ export default {
   components: {
     HelloWorld,
     MyCard
+  },
+
+  mounted: function() {
+
+    // populate degree dropdown menu
+    $.getJSON('/data.json', function (data) {
+        // Work with your JSON data here..
+        console.log("degree " + data[0].degree);
+        console.log("filepath " + data[0].filepath)
+        // Populate drop down menu with degrees from JSON
+        var $degreeSelector = $("#degree-select");
+        $degreeSelector.empty();
+        $degreeSelector.append("<option selected>Select a degree program</option>")
+        $.each(data, function(key, value) {
+            $degreeSelector.append("<option>" + value.degree + "</option>")
+        });
+    });
+
   }
+
 }
 </script>
 
