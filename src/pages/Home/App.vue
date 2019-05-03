98ea5891961ee66cf4ef6bb65a87b6e57771ea9f
bexar <bexar@uw.edu>
2019-05-03 09:38:21 -0600

    removes center alignment

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -76,7 +76,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  //text-align: center;
   color: #333;
   margin-top:1.3em;
 }
